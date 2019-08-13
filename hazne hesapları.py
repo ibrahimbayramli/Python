8b0292fd7dca6ae14a2b9c42fa510b5b13006f90
ibrahimbayramli <54009509+ibrahimbayramli@users.noreply.github.com>
2019-08-14 02:46:00 +0300

    ibrahimbayramli

--- hazne hesapları.py
+++ hazne hesapları.py
@@ -4,11 +4,12 @@ import time
 print("Hazne veri hesaplayıcıya hoşgeldiniz.")
 time.sleep(3)
 print("Programdan çıkmak için herhangi bir harf tuşuna basınız.")
-time.sleep(3)
+#time.sleep(3)
 print("Hesaplamalarda sürtünme kayıpları dışındaki bütün kayıplar ihmal edilmiştir. g=9.81 m/sn^2 alınmıştır.")
 time.sleep(3)
 print("Hesaplamalarda girilecek değerler su yüzü kotunda ve boru uzunluğunda (m), hızda (m/sn), debide (m^3/sn), boru çapında (mm)")
-time.sleep(5)
+time.sleep(5) 
+
 print("Hesaplama yapılabilmesi için girilmesi gereken veri setleri: ","1) ZA, L, V, D, f (hk, Q ve ZB'yi bulmak için.)")
 print(len("Hesaplama yapılabilmesi için girilmesi gereken veri setleri: ")*" ","2) ZB, L, V, D, f (hk, Q ve ZA'yı bulmak için.)")
 print(len("Hesaplama yapılabilmesi için girilmesi gereken veri setleri: ")*" ","3) ZA, L, V, Q, f (hk, D ve ZB'yi bulmak için.)")
@@ -50,10 +51,9 @@ while True:
        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
        time.sleep(1)
        a=za-(l*v**2*f)/((d/1000)*19.62)                         #ZB kotunu hesaplar
-
        print("ZB:", a ,"m")                                     #ZB kotunun çıktısı
        print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
-       print(("Q: ", v*(math.pi*(d/1000)**2)/4),"m^3/sn")       #Debi (Q) çıktısı
+       print("Q: ", v*(math.pi*(d/1000)**2)/4,"m^3/sn")         #Debi (Q) çıktısı
 
 
 
@@ -83,11 +83,10 @@ while True:
 
        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
        time.sleep(1)
-       a=zb+(l*v**2*f)/((d/1000)*19.62)                         #ZA kotunu hesaplar
-
-       print("ZA:", a ,"m")                                     #ZA kotunun çıktısı
-       print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
-       print(("Q: ", v*(math.pi*(d/1000)**2)/4),"m^3/sn")       #Debi (Q) çıktısı
+       a=zb+(l*v**2*f)/((d/1000)*19.62)                          #ZA kotunu hesaplar
+       print("ZA:", a ,"m")                                      #ZA kotunun çıktısı
+       print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")             #hk çıktısı
+       print("Q: ", v*(math.pi*(d/1000)**2)/4,"m^3/sn")          #Debi (Q) çıktısı
 
 
 
@@ -116,11 +115,11 @@ while True:
 
        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
        time.sleep(1)
+       d=float((4*q/v*math.pi)**0.5)                            #D yi bulmak için yapılan hesap
        a=za-(l*v**2*f)/((d/1000)*19.62)                         #ZB kotunu hesaplar
-
        print("ZB:", a ,"m")                                     #ZB kotunun çıktısı
        print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
-       print(("D: ", q/v ,"mm"))                                #Boru çapı (D) çıktısı
+       print("D: ", d ,"mm")                                    #Boru çapı (D) çıktısı
 
 
 
@@ -150,43 +149,42 @@ while True:
        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
        time.sleep(1)
        a=zb+(l*v**2*f)/((d/1000)*19.62)                         #ZA kotunu hesaplar
-
        print("ZB:", a ,"m")                                     #ZA kotunun çıktısı
        print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
-       print(("D: ", q/v ,"mm"))                                #Boru çapı (D) çıktısı
+       print("D: ", q/v ,"mm")                                  #Boru çapı (D) çıktısı
 
 
 
 
-       if x==5:
+    if x==5:
+        za=float(input("ZA değerini giriniz: "))
+        if za<0:
+            print("Lütfen pozitif bir değer giriniz.")
             za=float(input("ZA değerini giriniz: "))
-            if za<0:
-                print("Lütfen pozitif bir değer giriniz.")
-                za=float(input("ZA değerini giriniz: "))
+        l=float(input("Boru boyunu (L) giriniz: "))
+        if l<0:
+            print("Lütfen pozitif bir değer giriniz.")
             l=float(input("Boru boyunu (L) giriniz: "))
-            if l<0:
-                print("Lütfen pozitif bir değer giriniz.")
-                l=float(input("Boru boyunu (L) giriniz: "))
+        d=float(input("Boru çapını (D) giriniz: "))
+        if d<0:
+            print("Lütfen pozitif bir değer giriniz.")
             d=float(input("Boru çapını (D) giriniz: "))
-            if d<0:
-                print("Lütfen pozitif bir değer giriniz.")
-                d=float(input("Boru çapını (D) giriniz: "))
-            q=float(input("Boru çapını (D) giriniz: "))
-            if q<0:
-                print("Lütfen pozitif bir değer giriniz.")
-                q=float(input("Borudaki debiyi (Q) giriniz: "))
+        q=float(input("Borudaki debiyi (Q) giriniz:"))
+        if q<0:
+            print("Lütfen pozitif bir değer giriniz.")
+            q=float(input("Borudaki debiyi (Q) giriniz: "))
+        f=float(input("Sürtünme katsayısını (f) giriniz: "))
+        if f<0:
+            print("Lütfen pozitif bir değer giriniz.")
             f=float(input("Sürtünme katsayısını (f) giriniz: "))
-            if f<0:
-                print("Lütfen pozitif bir değer giriniz.")
-                f=float(input("Sürtünme katsayısını (f) giriniz: "))
 
-            print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
-            time.sleep(1)
-            a=za-((l*(q/(math.pi*d**2/4))**2*f)/((d/1000)*19.62))    #ZB kotunu hesaplar
-
-            print("ZB:", a ,"m")                                     #ZB kotunun çıktısı
-            print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
-            print(("D: ", q/v ,"mm"))                                #Boru çapı (D) çıktısı
+        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
+        time.sleep(1)
+        v=float(q/((math.pi*d**2)/4))                            #Borudaki hızı (V) heaplar
+        a=za-((l*(q/(math.pi*d**2/4))**2*f)/((d/1000)*19.62))    #ZB kotunu hesaplar
+        print("ZB:", a ,"m")                                     #ZB kotunun çıktısı
+        print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")            #hk çıktısı
+        print("V: ", q/((math.pi*d**2)/4) ,"m/sn")               #Boru hızı (V) çıktısı
 
 
 
@@ -215,11 +213,11 @@ while True:
 
        print("Sonuçlar hesaplanıyor lütfen bekleyiniz...")
        time.sleep(1)
+       v=float(q/((math.pi*d**2)/4))                                #Borudaki hızı (V) heaplar
        a=zb+((l*(q/(math.pi*d**2/4))**2*f)/((d/1000)*19.62) )       #ZA kotunu hesaplar
-
        print("ZA:", a ,"m")                                         #ZA kotunun çıktısı
        print("hk: ",(l*v**2*f)/((d/1000)*19.62),"m")                #hk çıktısı
-       print(("D: ", q/v ,"mm"))                                    #Boru çapı (D) çıktısı
+       print("D: ", q/v ,"mm")                                      #Boru çapı (D) çıktısı
 
 
 
@@ -303,7 +301,7 @@ while True:
         if zb<0 :
            print("Lütfen pozitif bir değer giriniz.")
            zb=float(input("ZB değerini giriniz: "))
-        b=za-zb
+        b=za-zb          #b diğer bir değişle hk yük kaybına eşit
         if b<0:
             print(za,"<",zb,"olduğu için ZA ve ZB değerlerini yeniden giriniz")
             za=float(input("ZA değerini giriniz: "))
